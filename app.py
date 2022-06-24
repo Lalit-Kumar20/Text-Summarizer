2947a7efd277d4dda04518986a0e8f4d210e1098
Lalit-Kumar20 <lalitk2282k@gmail.com>
2022-06-24 20:06:52 +0530

    tf-idf

--- app.py
+++ app.py
@@ -1,39 +1,173 @@
 from flask import Flask,render_template,request
 import nltk,re
 import heapq
-def sm(article_text):
-    article_text = article_text.lower()
-    clean_text = re.sub('[^a-zA-Z]', ' ', article_text)
-    clean_text = re.sub('\s+', ' ', clean_text)
-    sentence_list = nltk.sent_tokenize(article_text)
-    stopwords = nltk.corpus.stopwords.words('english')
-    word_frequencies = {}
-    for word in nltk.word_tokenize(clean_text):
-       if word not in stopwords:
-          if word not in word_frequencies:
-             word_frequencies[word] = 1
-          else:
-             word_frequencies[word] += 1
+def _create_frequency_matrix(sentences):
+    frequency_matrix = {}
+    stopWords = set(stopwords.words("english"))
+    ps = PorterStemmer()
 
-    maximum_frequency = max(word_frequencies.values())
-    for word in word_frequencies:
-        word_frequencies[word] = word_frequencies[word] / maximum_frequency
-    sentence_scores = {}
-    for sentence in sentence_list:
-        for word in nltk.word_tokenize(sentence):
-            if word in word_frequencies and len(sentence.split(' ')) < 30:
-                if sentence not in sentence_scores:
-                    sentence_scores[sentence] = word_frequencies[word]
-                else:
-                    sentence_scores[sentence] += word_frequencies[word]
+    for sent in sentences:
+        freq_table = {}
+        words = word_tokenize(sent)
+        for word in words:
+            word = word.lower()
+            word = ps.stem(word)
+            if word in stopWords:
+                continue
 
-    summary = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
-    return " ".join(summary)
+            if word in freq_table:
+                freq_table[word] += 1
+            else:
+                freq_table[word] = 1
+
+        frequency_matrix[sent[:15]] = freq_table
+
+    return frequency_matrix
+
+def _create_tf_matrix(freq_matrix):
+    tf_matrix = {}
+
+    for sent, f_table in freq_matrix.items():
+        tf_table = {}
+
+        count_words_in_sentence = len(f_table)
+        for word, count in f_table.items():
+            tf_table[word] = count / count_words_in_sentence
+
+        tf_matrix[sent] = tf_table
+
+    return tf_matrix
+
+def _create_documents_per_words(freq_matrix):
+    word_per_doc_table = {}
+
+    for sent, f_table in freq_matrix.items():
+        for word, count in f_table.items():
+            if word in word_per_doc_table:
+                word_per_doc_table[word] += 1
+            else:
+                word_per_doc_table[word] = 1
+
+    return word_per_doc_table
+
+def _create_idf_matrix(freq_matrix, count_doc_per_words, total_documents):
+    idf_matrix = {}
+
+    for sent, f_table in freq_matrix.items():
+        idf_table = {}
+
+        for word in f_table.keys():
+            idf_table[word] = math.log10(total_documents / float(count_doc_per_words[word]))
+
+        idf_matrix[sent] = idf_table
+
+    return idf_matrix
+
+def _create_tf_idf_matrix(tf_matrix, idf_matrix):
+    tf_idf_matrix = {}
+
+    for (sent1, f_table1), (sent2, f_table2) in zip(tf_matrix.items(), idf_matrix.items()):
+
+        tf_idf_table = {}
+
+        for (word1, value1), (word2, value2) in zip(f_table1.items(),
+                                                    f_table2.items()):  # here, keys are the same in both the table
+            tf_idf_table[word1] = float(value1 * value2)
+
+        tf_idf_matrix[sent1] = tf_idf_table
+
+    return tf_idf_matrix
+
+def _score_sentences(tf_idf_matrix) -> dict:
+    
+
+    sentenceValue = {}
+
+    for sent, f_table in tf_idf_matrix.items():
+        total_score_per_sentence = 0
+
+        count_words_in_sentence = len(f_table)
+        for word, score in f_table.items():
+            total_score_per_sentence += score
+
+        sentenceValue[sent] = total_score_per_sentence / count_words_in_sentence
+
+    return sentenceValue
+
+def _find_average_score(sentenceValue) -> int:
+
+    sumValues = 0
+    for entry in sentenceValue:
+        sumValues += sentenceValue[entry]
+
+    # Average value of a sentence from original summary_text
+    average = (sumValues / len(sentenceValue))
+
+    return average
+
+def _generate_summary(sentences, sentenceValue, threshold):
+    sentence_count = 0
+    summary = ''
+
+    for sentence in sentences:
+        if sentence[:15] in sentenceValue and sentenceValue[sentence[:15]] >= (threshold):
+            summary += " " + sentence
+            sentence_count += 1
+
+    return summary
+
+
+import math
+import nltk
+from nltk import sent_tokenize, word_tokenize, PorterStemmer
+from nltk.corpus import stopwords    
+    
+'''
+We already have a sentence tokenizer, so we just need 
+to run the sent_tokenize() method to create the array of sentences.
+'''
+# 1 Sentence Tokenize
+def summary_generator(text):
+    sentences = sent_tokenize(text)
+    total_documents = len(sentences)
+    #print(total_documents)
+
+    # 2 Create the Frequency matrix of the words in each sentence.
+    freq_matrix = _create_frequency_matrix(sentences)
+    #print(freq_matrix)
+
+# 3 Calculate TermFrequency and generate a matrix
+    tf_matrix = _create_tf_matrix(freq_matrix)
+#print(tf_matrix)
+
+# 4 creating table for documents per words
+    count_doc_per_words = _create_documents_per_words(freq_matrix)
+#print(count_doc_per_words)
+
+# 5 Calculate IDF and generate a matrix
+    idf_matrix = _create_idf_matrix(freq_matrix, count_doc_per_words, total_documents)
+#print(idf_matrix)
+
+# 6 Calculate TF-IDF and generate a matrix
+    tf_idf_matrix = _create_tf_idf_matrix(tf_matrix, idf_matrix)
+#print(tf_idf_matrix)
+
+# 7 Important Algorithm: score the sentences
+    sentence_scores = _score_sentences(tf_idf_matrix)
+#print(sentence_scores)
+
+# 8 Find the threshold
+    threshold = _find_average_score(sentence_scores)
+#print(threshold)
+
+# 9 Important Algorithm: Generate the summary
+    summary = _generate_summary(sentences, sentence_scores, 1.3 * threshold)
+    return summary
 app = Flask(__name__)
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        return render_template("index.html",data=request.form['inp'],val=sm(request.form['inp']))    
+        return render_template("index.html",data=request.form['inp'],val=summary_generator(request.form['inp']))    
     else:
         return render_template("index.html",data="",val="")
 if __name__=="__main__":
